dbd318d131afe1cd02bca6f4603ef96bc1eb41fe
nbeers22 <nbeers22@gmail.com>
2018-09-05 10:00:07 -0500

    Added templates to be used on taxonomy pages. Added directive for foundation dropdown menu

--- themes/madx/template-parts/taxonomy/residential-solar.php
+++ themes/madx/template-parts/taxonomy/residential-solar.php
@@ -10,36 +10,9 @@
 			</div>
 		</div>
 		<div class="grid-x grid-margin-x grid-margin-y">
-			<div class="medium-4 cell text-center">
-				<i class="far fa-sun yellow"></i>
-				<h5 class="blue">Improve Safety</h5>
-				<p>Increase your family's safety with solar control film, which blocks 99% of the sun's harmful UV rays</p>
-			</div>
-			<div class="medium-4 cell text-center">
-				<i class="far fa-sun yellow"></i>
-				<h5 class="blue">Save Money</h5>
-				<p>Solar control film cuts down heat in the summer and reflects it back into your home in the winter, reducing your energy bills</p>
-			</div>
-			<div class="medium-4 cell text-center">
-				<i class="far fa-sun yellow"></i>
-				<h5 class="blue">Control Glare</h5>
-				<p>Reduce annoying glare that penetrates through windows, and improve visibility on TV and devices with solar control film</p>
-			</div>
-			<div class="medium-4 cell text-center">
-				<i class="far fa-sun yellow"></i>
-				<h5 class="blue">Prevent Fading</h5>
-				<p>Constant exposure to the sun can speed up the fading of your floors, upholstery, and home furnishings &mdash; solar control film deters it</p>
-			</div>
-			<div class="medium-4 cell text-center">
-				<i class="far fa-sun yellow"></i>
-				<h5 class="blue">Accent Looks</h5>
-				<p>Solar control film is available in many shades and hues, so you can enhance your home's look with your personal style</p>
-			</div>
-			<div class="medium-4 cell text-center">
-				<i class="far fa-sun yellow"></i>
-				<h5 class="blue">Improve Privacy</h5>
-				<p>In addition to offering protection, solar control film can improve privacy during the day, preventing outsiders from looking in</p>
-			</div>
+
+			<?php get_template_part('template-parts/taxonomy-benefits'); ?>
+
 		</div>
 	</div>
 </section>
@@ -59,47 +32,7 @@
 	</div>
 </section>
 
-<section class="taxonomy-faqs">
-	<div class="grid-container">
-		<div class="grid-x">
-			<div class="medium-10 large-8 medium-offset-1 large-offset-2 text-center">
-				<h3 class="blue">FAQs</h3>
-				<aside class="yellow-underline center"></aside>
-				<p class="subhead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia neque, pariatur quam recusandae assumenda laudantium nostrum voluptate. Animi alias perspiciatis eum accusantium soluta</p>
-			</div>
-		</div>
-		<div class="grid-x">
-			<div class="medium-10 medium-offset-1 cell">
-			  <div class="grid-x grid-margin-x grid-margin-y">
-
-			  <!-- Query custom post type 'faq' filtered by taxonomy faq_taxonomies -->
-				<?php
-					$args = array(
-						'post_type' => 'faq',
-						'tax_query' => array(
-							array(
-								'taxonomy' => 'faq_taxonomies',
-								'field'    => 'slug',
-								'terms'    => 'residential',
-							),
-						),
-					);
-					$query = new WP_Query( $args );
-					while ( $query->have_posts() ) : $query->the_post();
-				?>
-				
-				<div class="medium-6 cell">
-					<h5><?php the_title(); ?></h5>
-					<?php the_content(); ?>
-				</div>
-				
-				<?php endwhile; wp_reset_postdata(); ?>
-					
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
+<?php get_template_part('/template-parts/taxonomy-faqs'); ?>
 
 <section class="film-selector">
 	<div class="grid-container">
