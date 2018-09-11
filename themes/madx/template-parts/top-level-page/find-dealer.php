17cab58e554092f796dbbc1abcb98a6f35537f01
nbeers22 <nbeers22@gmail.com>
2018-09-11 10:00:31 -0500

    Added find dealer info for taxonomy pages. Did active menu item css

--- themes/madx/template-parts/top-level-page/find-dealer.php
+++ themes/madx/template-parts/top-level-page/find-dealer.php
@@ -1,10 +1,15 @@
-<section class="find-dealer" style="background-image: url(<?php the_field('find_dealer_background_image'); ?>);">
+<?php 
+	$current_page = get_page_by_path(get_post_type());
+	$page_id = $current_page->ID;
+ ?>
+
+<section class="find-dealer" style="background-image: url(<?php the_field('find_dealer_background_image',$page_id); ?>);">
 	<div class="grid-container">
 		<div class="grid-x">
 			<div class="medium-8 large-7 cell">
-				<h2 class="white"><?php the_field('find_dealer_heading'); ?></h2>
+				<h2 class="white"><?php the_field('find_dealer_heading',$page_id); ?></h2>
 				<aside class="yellow-underline left"></aside>
-				<p class="white"><?php the_field('find_dealer_subhead'); ?></p>
+				<p class="white"><?php the_field('find_dealer_subhead',$page_id); ?></p>
 				
 				<find-dealer-form></find-dealer-form>
 
