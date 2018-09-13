cd98d150b2dbd863436a418d6bc10e910373d757
nbeers22 <nbeers22@gmail.com>
2018-09-13 14:52:47 -0500

    Completed blog single header and search form and basic layout of single.php

--- themes/madx/single.php
+++ themes/madx/single.php
@@ -7,18 +7,52 @@
  */
 
 get_header(); ?>
+<?php while ( have_posts() ) : the_post(); ?>
 
-<?php get_template_part( 'template-parts/featured-image' ); ?>
-<div class="main-container">
-	<div class="main-grid">
-		<main class="main-content">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'template-parts/content', '' ); ?>
-				<?php the_post_navigation(); ?>
-				<?php comments_template(); ?>
-			<?php endwhile; ?>
-		</main>
-		<?php get_sidebar(); ?>
+<section class="page-hero" style="background-image: url(<?php the_post_thumbnail_url( 'full' ); ?>);">
+
+	<?php get_template_part('template-parts/menus/blog-header-menu'); ?>
+	<?php $cat = get_the_category(); ?>
+
+	<div class="grid-container">
+		<div class="grid-x">
+			<div class="large-10 medium-8 cell">
+				<h1 class="blue"><?php the_title(); ?></h1>
+				<ul class="post-meta absolute">
+					<li><i class="fal fa-calendar-alt"></i> &nbsp; <?php the_date(); ?></li>
+					<li><i class="fal fa-folder-open"></i> <?php echo $cat[0]->name; ?></li>
+					<li>
+						<ul class="social">
+							<li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+							<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+							<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
 	</div>
-</div>
+</section>
+
+<section class="post-content">
+	<div class="grid-container">
+		<div class="grid-x grid-margin-x">
+			<div class="medium-7 cell">
+				
+				<?php the_content(); ?>
+
+			</div>
+			<div class="medium-4 medium-offset-1 cell">
+
+				<?php get_sidebar(); ?>
+
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php endwhile; ?>
+
+
 <?php get_footer();
--- themes/madx/template-parts/content.php
+++ themes/madx/template-parts/content.php
@@ -10,6 +10,22 @@
 
 ?>
 
+<section class="page-hero" style="<?php the_post_thumbnail_url( 'full' ); ?>">
+
+	<?php get_template_part('template-parts/menus/blog-header-menu'); ?>
+
+	<div class="grid-container">
+		<div class="grid-x">
+			<div class="large-8 medium-10 cell">
+				<h1 class="blue"><?php the_field('page_hero_title'); ?></h1>
+				<aside class="yellow-underline left"></aside>
+				<p class="subhead"><?php the_field('page_hero_subhead'); ?></p>
+				<a href="<?php the_field('page_hero_left_button_link'); ?>" class="btn-yellow solid"><?php the_field('page_hero_left_button_text'); ?></a>&nbsp;&nbsp;&nbsp;<a href="<?php the_field('page_hero_right_button_link'); ?>" class="btn-yellow border"><?php the_field('page_hero_right_button_text'); ?></a>
+			</div>
+		</div>
+	</div>
+</section>
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header>
 	<?php
