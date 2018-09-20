0ce754549dc1757594f06116768ead69ff79debf
nbeers22 <nbeers22@gmail.com>
2018-09-20 15:05:54 -0500

    Completed auto section for windshield protection and solar. Added testimonials block and post type

---
+++ themes/madx/template-parts/taxonomy/commercial.php
@@ -0,0 +1,24 @@
+<?php get_template_part('template-parts/menus/commercial-header-menu'); ?>
+<?php $term = get_queried_object(); ?>
+
+<section class="taxonomy-intro">
+	<div class="grid-container">
+		<div class="grid-x">
+			<div class="large-8 medium-10 medium-offset-1 large-offset-2 cell text-center">
+				<h1 class="blue"><?php the_field('intro_heading',$term); ?></h1>
+				<aside class="yellow-underline center"></aside>
+				<p class="subhead"><?php the_field('intro_subhead',$term); ?></p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="tax-posts" class="taxonomy-products">
+	<div class="grid-container">
+
+		<tax-term-posts></tax-term-posts>
+
+	</div>
+</section>
+
+<?php get_template_part('/template-parts/top-level-page/find-dealer'); ?>
