11ffe92c949540b77c316a2d2b499e8f81c96c33
nbeers22 <nbeers22@gmail.com>
2018-09-24 14:39:45 -0500

    Several changes to make site more tablet/mobile friendly

--- themes/madx/footer.php
+++ themes/madx/footer.php
@@ -3,77 +3,80 @@
 <footer class="footer">
   <div class="grid-container">
   	<div class="grid-x grid-margin-x grid-margin-y">
-  		<div class="small-6 medium-4 large-2 cell">
-  			<img src="<?php bloginfo('template_directory'); ?>/dist/assets/images/logo-blue.png" alt="Madico">
-  		</div>
-  		<div class="small-6 medium-4 large-2 cell">
-  			<ul>
-  				<li class="heading"><h6 class="blue">Company</h6></li>
-  				<li><a href="#">Overview</a></li>
-  				<li><a href="#">Our Businesses</a></li>
-  				<li><a href="#">Madico Cares</a></li>
-  				<li><a href="#">News</a></li>
-  				<li><a href="#">Press Releases</a></li>
-  				<li><a href="#">Blog</a></li>
-  				<li><a href="#">Careers</a></li>
-  				<li><a href="#">FAQs</a></li>
-  			</ul>
-  		</div>
-  		<div class="small-6 medium-4 large-2 cell">
-  			<ul>
-  				<li class="heading"><h6 class="blue">Films</h6></li>
-  				<li><a href="#">Auto</a></li>
-  				<li><a href="#">Residential</a></li>
-  				<li><a href="#">Commercial</a></li>
-  				<li><a href="#">Safety &amp; Security</a></li>
-  				<li><a href="#">Performance</a></li>
-  				<li><a href="#">Warranty</a></li>
-  			</ul>
-  		</div>
-  		<div class="small-6 medium-4 large-2 cell">
-  			<ul>
-  				<li class="heading"><h6 class="blue">Specialty</h6></li>
-  				<li><a href="#">Spcialty Solutions</a></li>
-  				<li><a href="#">Industries</a></li>
-  				<li><a href="#">Products</a></li>
-  				<li><a href="#">Applications</a></li>
-  				<li><a href="#">Capabilities</a></li>
-  				<li><a href="#">Case Studies</a></li>
-  				<li><a href="#">Start a Project</a></li>
-  			</ul>
-  		</div>
-  		<div class="small-6 medium-4 large-2 cell">
-  			<ul>
-  				<li class="heading"><h6 class="blue">Dealers</h6></li>
-  				<li><a href="#">Dealer Portal</a></li>
-  				<li><a href="#">Become a Dealer</a></li>
-  				<li><a href="#">Products</a></li>
-  				<li><a href="#">Madico U</a></li>
-  				<li><a href="#">Dealer Programs</a></li>
-  				<li><a href="#">Distribution</a></li>
-  				<li><a href="#">FAQs</a></li>
-  			</ul>
-  		</div>
-  		<div class="small-6 medium-4 large-2 cell">
-  			<ul>
-  				<li class="heading"><h6 class="blue">Connect</h6></li>
-  				<li><a href="#"><i class="fab fa-facebook"></i>&nbsp; Facebook</a></li>
-  				<li><a href="#"><i class="fab fa-twitter"></i>&nbsp; Twitter</a></li>
-  				<li><a href="#"><i class="fab fa-google-plus-g"></i>&nbsp; Google Plus</a></li>
-  				<li><a href="#"><i class="fab fa-linkedin"></i>&nbsp; LinkedIn</a></li>
-  				<li><a href="#"><i class="fab fa-youtube"></i>&nbsp; YouTube</a></li>
-  				<li><a href="#"><i class="fab fa-instagram"></i>&nbsp; Instagram</a></li>
-  			</ul>
-  		</div>
-  		<div class="small-12 cell text-center credits">
-  			<address>9251 Belcher Road N. Pinellas Park, FL 33782</address>
-  			<ul class="privacy">
-  				<li><a href="#">Contact Us</a></li>&nbsp; |
-  				<li><a href="#">Terms &amp; Conditions</a></li>&nbsp; |
-  				<li><a href="#">Privacy Policy</a></li>
-  			</ul>
-  			<p>Copyright <?php echo date('Y'); ?> Madico<sup>&reg;</sup>, Inc.</p>
-  		</div>
+      <div class="small-10 small-offset-1 large-12 large-offset-0">
+    		<div class="grid-x grid-margin-x grid-margin-y">
+          <div class="small-6 medium-4 large-2 cell">
+            <img src="<?php bloginfo('template_directory'); ?>/dist/assets/images/logo-blue.png" alt="Madico">
+          </div>
+          <div class="small-6 medium-4 large-2 cell">
+            <ul>
+              <li class="heading"><h6 class="blue">Company</h6></li>
+              <li><a href="/about">Overview</a></li>
+              <li><a href="/about/our-businesses">Our Businesses</a></li>
+              <li><a href="/about/madico-cares">Madico Cares</a></li>
+              <li><a href="/about/news">News</a></li>
+              <li><a href="/about/press-releases">Press Releases</a></li>
+              <li><a href="/blog">Blog</a></li>
+              <li><a href="/about/careers">Careers</a></li>
+              <li><a href="#">FAQs</a></li>
+            </ul>
+          </div>
+          <div class="small-6 medium-4 large-2 cell">
+            <ul>
+              <li class="heading"><h6 class="blue">Films</h6></li>
+              <li><a href="/auto">Auto</a></li>
+              <li><a href="/residential">Residential</a></li>
+              <li><a href="/commercial">Commercial</a></li>
+              <li><a href="/safety-security">Safety &amp; Security</a></li>
+              <li><a href="/performance">Performance</a></li>
+              <li><a href="/warranty">Warranty</a></li>
+            </ul>
+          </div>
+          <div class="small-6 medium-4 large-2 cell">
+            <ul>
+              <li class="heading"><h6 class="blue">Specialty</h6></li>
+              <li><a href="/specialty-solutions">Specialty Solutions</a></li>
+              <li><a href="#">Industries</a></li>
+              <li><a href="#">Products</a></li>
+              <li><a href="#">Applications</a></li>
+              <li><a href="#">Capabilities</a></li>
+              <li><a href="#">Start a Project</a></li>
+            </ul>
+          </div>
+          <div class="small-6 medium-4 large-2 cell">
+            <ul>
+              <li class="heading"><h6 class="blue">Dealers</h6></li>
+              <li><a href="#">Dealer Portal</a></li>
+              <li><a href="#">Become a Dealer</a></li>
+              <li><a href="#">Products</a></li>
+              <li><a href="#">Madico U</a></li>
+              <li><a href="#">Dealer Programs</a></li>
+              <li><a href="#">Distribution</a></li>
+              <li><a href="#">FAQs</a></li>
+            </ul>
+          </div>
+          <div class="small-6 medium-4 large-2 cell">
+            <ul>
+              <li class="heading"><h6 class="blue">Connect</h6></li>
+              <li><a href="#"><i class="fab fa-facebook"></i>&nbsp; Facebook</a></li>
+              <li><a href="#"><i class="fab fa-twitter"></i>&nbsp; Twitter</a></li>
+              <li><a href="#"><i class="fab fa-google-plus-g"></i>&nbsp; Google Plus</a></li>
+              <li><a href="#"><i class="fab fa-linkedin"></i>&nbsp; LinkedIn</a></li>
+              <li><a href="#"><i class="fab fa-youtube"></i>&nbsp; YouTube</a></li>
+              <li><a href="#"><i class="fab fa-instagram"></i>&nbsp; Instagram</a></li>
+            </ul>
+          </div>
+          <div class="small-12 cell text-center credits">
+            <address>9251 Belcher Road N. Pinellas Park, FL 33782</address>
+            <ul class="privacy">
+              <li><a href="/contact">Contact Us</a></li>&nbsp; |
+              <li><a href="terms-and-conditions">Terms &amp; Conditions</a></li>&nbsp; |
+              <li><a href="/privacy-policy">Privacy Policy</a></li>
+            </ul>
+            <p>Copyright <?php echo date('Y'); ?> Madico<sup>&reg;</sup>, Inc.</p>
+          </div>
+        </div>
+      </div>
   	</div>
   </div>
 </footer>
--- themes/madx/page-benefits.php
+++ themes/madx/page-benefits.php
@@ -7,7 +7,7 @@ get_header(); ?>
 <section class="benefits-intro">
 	<div class="grid-container">
 		<div class="grid-x">
-			<div class="medium-10 medium-offset-1 large-8 large-offset-2 cell text-center">
+			<div class="small-10 small-offset-1 large-8 large-offset-2 cell text-center">
 				<h1 class="blue"><?php the_field('benefits_intro_heading'); ?></h1>
 				<aside class="yellow-underline center"></aside>
 				<p><?php the_field('benefits_intro_subhead'); ?></p>
@@ -19,8 +19,8 @@ get_header(); ?>
 <section class="benefits-energy" style="background-image: url(<?php the_field('benefits_energy_bg_image'); ?>);">
 	<div class="grid-container">
 		<div class="grid-x">
-			<div class="medium-5 medium-offset-1 large-3 cell">
-				<h3 class="blue"><?php the_field('benefits_energy_heading'); ?></h3>
+			<div class="medium-5 small-offset-1 small-10 large-3 cell">
+				<h2 class="blue"><?php the_field('benefits_energy_heading'); ?></h2>
 				<aside class="yellow-underline left"></aside>
 				<p><?php the_field('benefits_energy_subhead'); ?></p>
 			</div>
@@ -31,8 +31,8 @@ get_header(); ?>
 <section class="benefits-environment" style="background-image: url(<?php the_field('benefits_environment_bg_image'); ?>);">
 	<div class="grid-container">
 		<div class="grid-x">
-			<div class="medium-6 medium-offset-1 cell">
-				<h3 class="white"><?php the_field('benefits_environment_heading'); ?></h3>
+			<div class="medium-6 small-10 small-offset-1 cell">
+				<h2 class="white"><?php the_field('benefits_environment_heading'); ?></h2>
 				<aside class="yellow-underline left"></aside>
 				<p class="white"><?php the_field('benefits_environment_subhead'); ?></p>
 			</div>
@@ -43,8 +43,8 @@ get_header(); ?>
 <section class="benefits-fade" style="background-image: url(<?php the_field('benefits_fade_bg_image'); ?>);">
 	<div class="grid-container">
 		<div class="grid-x">
-			<div class="medium-7 medium-offset-4 large-6 large-offset-5 cell text-right">
-				<h3 class="white"><?php the_field('benefits_fade_heading'); ?></h3>
+			<div class="medium-7 medium-offset-4 small-10 small-offset-1 large-6 large-offset-5 cell text-right">
+				<h2 class="white"><?php the_field('benefits_fade_heading'); ?></h2>
 				<aside class="yellow-underline right"></aside>
 				<p class="white"><?php the_field('benefits_fade_subhead'); ?></p>
 			</div>
@@ -55,8 +55,8 @@ get_header(); ?>
 <section class="benefits-safety" style="background-image: url(<?php the_field('benefits_safety_bg_image'); ?>);">
 	<div class="grid-container">
 		<div class="grid-x">
-			<div class="medium-5 medium-offset-1 large-4 cell">
-				<h3 class="blue"><?php the_field('benefits_safety_heading'); ?></h3>
+			<div class="medium-5 small-10 small-offset-1 large-4 cell">
+				<h2 class="blue"><?php the_field('benefits_safety_heading'); ?></h2>
 				<aside class="yellow-underline left"></aside>
 				<p><?php the_field('benefits_safety_subhead'); ?></p>
 			</div>
@@ -67,8 +67,8 @@ get_header(); ?>
 <section class="benefits-appearance" style="background-image: url(<?php the_field('benefits_appearance_bg_image'); ?>);">
 	<div class="grid-container">
 		<div class="grid-x">
-			<div class="medium-7 medium-offset-4 large-6 large-offset-5 cell text-right">
-				<h3 class="white"><?php the_field('benefits_appearance_heading'); ?></h3>
+			<div class="medium-7 medium-offset-4 small-10 small-offset-1 large-6 large-offset-5 cell text-right">
+				<h2 class="white"><?php the_field('benefits_appearance_heading'); ?></h2>
 				<aside class="yellow-underline right"></aside>
 				<p class="white"><?php the_field('benefits_appearance_subhead'); ?></p>
 			</div>
@@ -76,12 +76,80 @@ get_header(); ?>
 	</div>
 </section>
 
-<section class="benefits-testimonials">
+<section class="taxonomy-testimonials">
 	<div class="grid-container">
 		<div class="grid-x">
-			<div class="medium-10 medium-offset-1 large-8 large-offset-2 cell text-center">
-				<h3 class="blue"><?php the_field('benefits_testimonials_heading'); ?></h3>
+			<div class="small-10 large-8 small-offset-1 large-offset-2 text-center">
+				<h3 class="blue">Customer Testimonials</h3>
 				<aside class="yellow-underline center"></aside>
+				<!-- Foundation 6 Carousel/Orbit -->
+				<div class="orbit" role="region" aria-label="Favorite Space Pictures" v-f-orbit>
+				  <div class="orbit-wrapper">
+				    <div class="orbit-controls">
+				      <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span><i class="fas fa-chevron-left"></i></button>
+				      <button class="orbit-next"><span class="show-for-sr">Next Slide</span><i class="fas fa-chevron-right"></i></button>
+				    </div>
+				    <div class="grid-x">
+				    	<div class="small-10 small-offset-1 medium-8 medium-offset-2 cell">
+						    <ul class="orbit-container">
+
+						    	  <!-- Query custom post type 'testimonials' filtered by taxonomy testimonials_taxonomies -->
+						    		<?php
+						    			$args = array(
+						    				'post_type' => 'testimonials',
+						    				'posts_per_page' => 1,
+						    				'tax_query' => array(
+						    					array(
+						    						'taxonomy' => 'testimonials_taxonomies',
+						    						'field'    => 'slug',
+						    						'terms'    => 'commercial',
+						    					),
+						    				),
+						    			);
+						    			$query = new WP_Query( $args );
+						    			while ( $query->have_posts() ) : $query->the_post();
+						    		?>
+						    		
+								      <li class="is-active orbit-slide">
+								        <div class="content"><?php the_content(); ?></div>
+								        <address class="name"><?php the_field('testimonial_name'); ?></address>
+								        <address class="location"><?php the_field('testimonial_location'); ?></address>
+								      </li>
+
+						    		
+						    		<?php endwhile; wp_reset_postdata(); ?>
+
+		    		    		<?php
+		    		    			$args = array(
+		    		    				'post_type' => 'testimonials',
+		    		    				'posts_per_page' => 99,
+		    		    				'offset' => 1,
+		    		    				'tax_query' => array(
+		    		    					array(
+		    		    						'taxonomy' => 'testimonials_taxonomies',
+		    		    						'field'    => 'slug',
+		    		    						'terms'    => 'commercial',
+		    		    					),
+		    		    				),
+		    		    			);
+		    		    			$query = new WP_Query( $args );
+		    		    			while ( $query->have_posts() ) : $query->the_post();
+		    		    		?>
+		    		    		
+		    				      <li class="orbit-slide">
+		    				        <div class="content"><?php the_content(); ?></div>
+		    				        <address class="name"><?php the_field('testimonial_name'); ?></address>
+		    				        <address class="location"><?php the_field('testimonial_location'); ?></address>
+		    				      </li>
+
+		    		    		
+		    		    		<?php endwhile; wp_reset_postdata(); ?>
+						      
+						    </ul>
+				    	</div>
+				    </div>
+				  </div>
+				</div>
 			</div>
 		</div>
 	</div>
