5ecee6e8348b108b7a1fd3550644fe20e8b18fc3
nbeers22 <nbeers22@gmail.com>
2018-09-27 14:19:41 -0500

    Finished growth & acquisitions and madico cares pages on all resolutions

--- themes/madx/functions.php
+++ themes/madx/functions.php
@@ -54,5 +54,8 @@ require_once( 'library/sticky-posts.php' );
 /** Configure responsive image sizes */
 require_once( 'library/responsive-images.php' );
 
+/** Add custom shortcodes */
+require_once( 'library/shortcodes.php' );
+
 /** If your site requires protocol relative url's for theme assets, uncomment the line below */
 // require_once( 'library/class-foundationpress-protocol-relative-theme-assets.php' );
