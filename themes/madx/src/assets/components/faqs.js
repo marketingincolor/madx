f3e878268a8828904f0211efe2e1b0c28c65e939
nbeers22 <nbeers22@gmail.com>
2018-10-07 09:15:47 -0500

    Completed all FAQ search/display functionality

--- themes/madx/src/assets/components/faqs.js
+++ themes/madx/src/assets/components/faqs.js
@@ -5,13 +5,14 @@ export default {
 		return{
 			searchText: '',
 	    faqPosts: [],
-	    taxonomyName: 'All',
+	    taxonomyName: 'all',
 	    postType: 'faq',
-	    activeIndex: 0
+	    activeIndex: 0,
+	    searchPosts: []
 		}
 	},
 	template: `<div class="small-10 small-offset-1 cell">
-							<form class="subhead" id="faq-search">
+							<form class="subhead" id="faq-search" v-on:submit.prevent="getSearchResults">
 								<div class="input-group relative">
 								  <span class="input-group-label">
 										<select v-model="taxonomyName">
@@ -21,15 +22,27 @@ export default {
 									    <option value="residential">Residential</option>
 									  </select>
 								  </span>
-								  <input class="input-group-field" :keyup="getSearchResults" type="text" v-model="searchText" placeholder="Start your search">
+								  <input class="input-group-field" type="text" v-model="searchText" v-on:keyup="getSearchResults" placeholder="Start your search">
 								  <button type="submit" class="absolute">
 								     <i class="fas fa-search"></i>
 								  </button>
 								</div>
 							</form>
 							<article id="faq-results">
-								<ul class="accordion" data-accordion>
+								<ul class="accordion" data-accordion v-if="faqPosts">
 								  <li @click="setActive(post,index)" class="accordion-item" :class="{ 'is-active': activeIndex === index }" data-accordion-item  v-for="(post,index) in faqPosts" :key="post.id">
+								    <div class="yellow-triangle"></div><a href="#!" class="accordion-title" v-html="'Q: ' + post.title.rendered"></a>
+								    <transition
+											name="custom-classes-transition"
+									    enter-active-class="animated fadeIn"
+									    leave-active-class="animated fadeOut"
+								    >
+											<div v-if="activeIndex === index"  class="accordion-content" data-tab-content v-html="'A. ' + post.content.rendered"></div>
+								    </transition>
+								  </li>
+								</ul>
+								<ul class="accordion" data-accordion v-if="searchPosts">
+								  <li @click="setActive(post,index)" class="accordion-item" :class="{ 'is-active': activeIndex === index }" data-accordion-item  v-for="(post,index) in searchPosts" :key="post.id">
 								    <div class="yellow-triangle"></div><a href="#!" class="accordion-title" v-html="'Q: ' + post.title.rendered"></a>
 								    <transition
 											name="custom-classes-transition"
@@ -52,7 +65,6 @@ export default {
 				axios
 				  .get(apiRoot + $this.postType)
 				  .then(function (response) {
-				    console.log(response.data)
 				    $this.faqPosts = response.data;
 				  }
 				)
@@ -62,43 +74,28 @@ export default {
 			},
 			getSearchResults: function(){
 				let $this = this;
-
-				if (this.taxonomyName == 'all'){
+				if (this.taxonomyName.toLowerCase() == 'all'){
 
 					axios
-					  .get(apiRoot + $this.postType)
+					  .get(apiRoot + $this.postType + '?search=' + $this.searchText)
 					  .then(function (response) {
+					    $this.searchPosts = response.data;
 					    console.log(response.data)
+					    $this.faqPosts = [];
 					  }
 					)
 
 				}else{
 
 					axios
-					  .get(apiRoot + $this.postType + '?_embed&filter['+ $this.postType +'_taxonomies]=' + taxonomyName)
+					  .get(apiRoot + $this.postType + '?_embed&filter['+ $this.postType +'_taxonomies]=' + $this.taxonomyName.toLowerCase() + '&search=' + $this.searchText)
 					  .then(function (response) {
-					    for (var i = 0; i < response.data.length; i++) {
-					    	if (response.data[i].link.includes($this.taxParentSlug)) {
-					    		$this.safetyPosts.push(response.data[i]);
-					    	}
-					    }
-					    $this.activeItem = $this.safetyPosts[0].title.rendered;
-					    let slug = $this.safetyPosts[0].slug;
-					    $this.getSafetyPostSingle(slug);
+					    $this.searchPosts = response.data;
+					    console.log(response.data)
+					    $this.faqPosts = [];
 					  }
 					)
 				}
-			},
-			getSafetyPostSingle: function(postSlug){
-				let $this    = this;
-
-				axios
-				  .get(apiRoot + $this.postType + '?_embed&slug=' + postSlug)
-				  .then(function (response) {
-				    $this.safetySinglePost = response.data;
-				    $this.activeItem = $this.safetySinglePost[0].title.rendered;
-				  }
-				)
 			}
 		}
 };
