8372e6b1b572146f67b0dfc652335d49537c9429
nbeers22 <nbeers22@gmail.com>
2018-10-12 14:52:10 -0400

    updated font awesome

--- themes/madx/template-parts/taxonomy/safety.php
+++ themes/madx/template-parts/taxonomy/safety.php
@@ -18,11 +18,6 @@
 				<p class="subhead"><?php the_field('intro_subhead',$term); ?></p>
 			</div>
 		</div>
-		<div class="grid-x">
-
-			<?php get_template_part('template-parts/taxonomy/benefits'); ?>
-
-		</div>
 	</div>
 </section>
 
@@ -36,64 +31,7 @@
 			</div>
 		</div>
 
-			<?php
-				$args = array(
-					'post_type' => 'safety',
-					'tax_query' => array(
-						array(
-							'taxonomy' => 'safety_taxonomies',
-							'field'    => 'slug',
-							'terms'    => $term->slug
-						),
-					),
-				);
-				$query = new WP_Query( $args );
-				while ( $query->have_posts() ) : $query->the_post();
-			?>
-
-				<div class="grid-x">
-					<div id="single-post" class="small-10 small-offset-1 cell module auto-height">
-						<img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
-						<div class="meta">
-							<div class="medium-12 cell">
-								<div class="grid-x grid-margin-x grid-margin-y">
-									<div class="medium-5 medium-offset-1 cell">
-										<h4 class="blue"><?php the_title(); ?></h4>
-										<div class="content"><?php the_content(); ?></div>
-										<?php if (get_field('pdf_link')) { ?>
-											<div class="grid-x grid-margin-y subhead">
-												<div class="medium-2 cell text-center">
-													<i class="fal fa-file-pdf"></i>
-												</div>
-												<div class="medium-10 cell">
-													<a href="<?php the_field('pdf_link'); ?>" target="_blank">Product Specs Doc</a>
-													<p>Specification Sheet Description</p>
-												</div>
-											</div>
-										<?php } ?>
-									</div>
-									<div class="medium-4 medium-offset-1 cell">
-										<h6>Product Benefits</h6>
-										<ul class="product-benefits">
-											<?php
-												if( have_rows('film_benefits') ):
-												  while ( have_rows('film_benefits') ): 
-												  	the_row();
-										  ?>
-													
-												<li><i class="fas fa-check"></i> &nbsp;<?php the_sub_field('benefit1'); ?></li>
-
-										<?php endwhile;endif; ?>
-											
-										</ul>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-
-			<?php endwhile; wp_reset_postdata(); ?>
+			<safety-posts></safety-posts>
 
 	</div>
 </section>
