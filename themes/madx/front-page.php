30d86413ccea6858ff37aee1775595b0a0def0c5
nbeers22 <nbeers22@gmail.com>
2018-10-13 10:18:08 -0400

    Updated header links acorss site

--- themes/madx/front-page.php
+++ themes/madx/front-page.php
@@ -7,27 +7,25 @@ get_header('home'); ?>
 	<div class="grid-container">
 		<div class="grid-x">
 			<div class="small-10 small-offset-1 medium-8 large-offset-0 cell">
-				<h1 class="white">Life, with Innovative Solutions</h1>
+				<h1 class="white"><?php the_field('home_hero_heading'); ?></h1>
 				<aside class="yellow-underline left"></aside>
-				<p class="white" style="margin-bottom:30px">One of the world’s leading manufacturers of innovative laminating and coating solutions, Madico<sup>®</sup> is renowned for high performance, multi-layer films engineered to exceed expectations. Whether you're looking for protection, safety, security, or enhanced comfort and beauty, we have products that fit your life, your style, and your needs.</p>
-				<a href="#!" class="btn-yellow solid">Play Video&nbsp;&nbsp;<i class="fas fa-play-circle"></i></a>&nbsp;<br class="show-for-small-only">
-				<a href="/about" class="btn-yellow border">More About Us</a>
+				<p class="white" style="margin-bottom:30px"><?php the_field('home_hero_subhead'); ?></p>
+				<a href="#!" class="btn-yellow solid"><?php the_field('home_hero_play_button_text'); ?>&nbsp;&nbsp;<i class="fas fa-play-circle"></i></a>&nbsp;<br class="show-for-small-only">
+				<a href="/about" class="btn-yellow border"><?php the_field('home_hero_about_button_text'); ?></a>
 			</div>
 		</div>
 	</div>
 </section>
 
-<jot-form form-id="82824093286160"></jot-form>
-
 <section class="dealer-callout">
 	<div class="grid-container">
 		<div class="grid-x">
 			<div class="small-10 small-offset-1 medium-6 cell text">
-				<h2 class="blue"><i class="fal fa-id-badge"></i>&nbsp; Are you a window film dealer?</h2>
-				<p>Browse through our Dealer Portal to learn more about our services and support through the Madico Experience, and if you're a Madico Dealer to purchase products online!</p>
+				<h2 class="blue"><i class="fal fa-id-badge"></i>&nbsp; <?php the_field('dealer_callout_heading'); ?></h2>
+				<p><?php the_field('dealer_callout_subhead'); ?></p>
 			</div>
 			<div class="small-10 small-offset-1 medium-4 medium-offset-0 cell text-right">
-				<a href="https://madicodealers.com/" class="btn-yellow solid">Tour Dealer Portal <i class="far fa-long-arrow-alt-right"></i></a>
+				<a href="<?php the_field('dealer_callout_button_link'); ?>" class="btn-yellow solid"><?php the_field('dealer_callout_heading'); ?> <i class="far fa-long-arrow-alt-right"></i></a>
 			</div>
 		</div>
 	</div>
@@ -48,35 +46,35 @@ get_header('home'); ?>
 					<div class="medium-6 large-3 cell module">
 						<a href="/residential"><img src="<?php bloginfo('template_directory'); ?>/dist/assets/images/black-pearl-car.png" alt="Auto"></a>
 						<div class="meta">
-							<a href="/residential"><h4 class="blue"><i class="fal fa-car"></i>&nbsp; Residential</h4></a>
+							<a href="/residential"><h4 class="blue"><i class="fal fa-home"></i>&nbsp; Residential</h4></a>
 							<p>Enjoy life in a better light with window film that saves energy, controls glare, improves safety, and enhances the appearance of your home.</p>
 						</div>
 					</div>
 					<div class="medium-6 large-3 cell module">
 						<a href="/commercial"><img src="<?php bloginfo('template_directory'); ?>/dist/assets/images/black-pearl-car.png" alt="Auto"></a>
 						<div class="meta">
-							<a href="/commercial"><h4 class="blue"><i class="fal fa-car"></i>&nbsp; Commercial</h4></a>
+							<a href="/commercial"><h4 class="blue"><i class="fal fa-building"></i>&nbsp; Commercial</h4></a>
 							<p>Installing window film is a savvy way to lower energy costs, reduce CO2 emissions, increase building safety, and improve your tenants' comfort.</p>
 						</div>
 					</div>
 					<div class="medium-6 large-3 cell module">
 						<a href="/safety-security"><img src="<?php bloginfo('template_directory'); ?>/dist/assets/images/black-pearl-car.png" alt="Auto"></a>
 						<div class="meta">
-							<a href="/safety-security"><h4 class="blue"><i class="fal fa-car"></i>&nbsp; Safety &amp; Security</h4></a>
+							<a href="/safety-security"><h4 class="blue"><i class="fal fa-shield"></i>&nbsp; Safety &amp; Security</h4></a>
 							<p>Our Safety & Security solutions help reduce the risk of crime, personal injury, and property damage and loss caused by natural disasters.</p>
 						</div>
 					</div>
 					<div class="medium-6 cell module">
 						<a href="/specialty-solutions"><img src="<?php bloginfo('template_directory'); ?>/dist/assets/images/specialty-module.png" alt="Specialty Solutions"></a>
 						<div class="meta">
-							<a href="/specialty-solutions"><h4 class="blue"><i class="fal fa-car"></i>&nbsp; Specialty Solutions</h4></a>
+							<a href="/specialty-solutions"><h4 class="blue"><i class="fal fa-plane-departure"></i></i>&nbsp; Specialty Solutions</h4></a>
 							<p>If you are an engineer or manufacturer in aerospace, healthcare, transportation, information display, or building materials, our Specialty Solutions have the expertise and experience to help develop highly customized, proprietary products to your specifications.</p>
 						</div>
 					</div>
 					<div class="medium-6 cell module">
 						<a href="/protectionpro"><img src="<?php bloginfo('template_directory'); ?>/dist/assets/images/device-protection-module.png" alt="Device Protection"></a>
 						<div class="meta">
-							<a href="/protectionpro"><h4 class="blue"><i class="fal fa-car"></i>&nbsp; Device Protection</h4></a>
+							<a href="/protectionpro"><h4 class="blue"><i class="fal fa-tablet-android-alt"></i></i>&nbsp; Device Protection</h4></a>
 							<p>ProtectionPro<sup>™</sup> by Madico<sup>®</sup> is an on-demand, high-performance screen and body protection system for all types of devices—from legacy to the newest release.</p>
 						</div>
 					</div>
