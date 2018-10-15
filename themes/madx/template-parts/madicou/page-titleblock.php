cb3cdba39b5a0bd743c0b34955c3c3745b9cc638
nbeers22 <nbeers22@gmail.com>
2018-10-15 15:26:56 -0400

    Many changes

--- themes/madx/template-parts/madicou/page-titleblock.php
+++ themes/madx/template-parts/madicou/page-titleblock.php
@@ -1,7 +1,7 @@
 <section class="titleblock-top">
 	<div class="grid-container">
 		<div class="grid-x">
-			<div class="small-10 small-offset-1 cell">
+			<div class="small-10 small-offset-1 large-8 large-offset-0 cell">
 				<h1 class="blue"><?php the_title(); ?></h1>
 				<aside class="yellow-underline left"></aside>
 				<p class="subhead"><?php the_content(); ?></p>		
