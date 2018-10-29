3f9a0e993036fe15d1ebb164c56186e18610d6fc
nbeers22 <nbeers22@gmail.com>
2018-10-29 13:14:43 -0500

    Mobile menu fixes everywhere except madicoU

--- themes/madx/header-home.php
+++ themes/madx/header-home.php
@@ -19,21 +19,43 @@
 	<body <?php body_class(); ?>>
 
 		<!-- #app is closed in footer.php -->
-		<div id="app">
+	<div id="app">
+
+		<div id="mobile-left-menu" class="mobile-left-menu absolute">
+			<div class="menu-container relative">
+				<button class="absolute" aria-label="<?php _e( 'Close Menu', 'madx' ); ?>" type="button" @click="mobileLeftMenuClose"><i class="fal fa-long-arrow-left"></i></button>
+				<ul class="menu vertical absolute">
+				  <li><a href="/auto"><i class="far fa-car"></i>&nbsp; Auto</a></li>
+				  <li><a href="/residential"><i class="far fa-home"></i>&nbsp; Residential</a></li>
+				  <li><a href="/commercial"><i class="far fa-building"></i>&nbsp; Commercial</a></li>
+				  <li><a href="/safety-security"><i class="fal fa-shield"></i>&nbsp; Safety &amp; Security</a></li>
+				  <li><a href="/specialty-solutions"><i class="fal fa-flask"></i>&nbsp; Specialty Solutions</a></li>
+				  <li><a href="/dealers"><i class="fal fa-id-badge"></i>&nbsp; Dealers</a></li>
+				  <li><a href="/madicou"><i class="far fa-building"></i>&nbsp; Madico U</a></li>
+				  <li><a href="/about"><i class="far fa-building"></i>&nbsp; Company</a></li>
+				</ul>
+			</div>
+		</div>
 
 	<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
 		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
 	<?php endif; ?>
 
 	<header id="top-header" class="site-header" role="banner">
-		<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle(); ?>>
-			<div class="title-bar-left">
-				<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
-				<span class="site-mobile-title title-bar-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-				</span>
+		<div class="mobile-nav">
+			<div class="hamburger-container">
+				<button id="hamburger" aria-label="<?php _e( 'Open Menu', 'madx' ); ?>" class="menu-icon" type="button" @click="mobileLeftMenuOpen"></button>
+			</div>
+			<div class="site-mobile-title title-bar-title">
+				<a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><img src="<?php bloginfo('template_directory'); ?>/dist/assets/images/madico-mobile-blue.png" alt="Madico"></a>
+			</div>
+			<div class="search-container">
+				<a id="search-toggle" data-toggle="mobile-search" @click="mobileMenuSearch" aria-open="false"><i class="fal fa-times hide"></i><i class="far fa-search"></i></a>
 			</div>
 		</div>
+    <div class="dropdown-pane right" id="mobile-search" v-drop-click>
+			<?php get_template_part('template-parts/search/page-searchform'); ?>
+    </div>
 		<nav class="site-navigation top-bar" role="navigation">
 			<div class="top-bar-left">
 				<div class="site-desktop-title top-bar-title">
@@ -83,7 +105,7 @@
 	</header>
 
 
-	<header class="site-header absolute" role="banner">
+	<header class="site-header absolute show-for-medium" role="banner">
 		<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle(); ?>>
 			<div class="title-bar-left">
 				<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
@@ -100,9 +122,6 @@
 				</div>
 			</div>
 			<div class="top-bar-right">
-				<?php //foundationpress_top_bar_r(); ?>
-				
-
 				<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
 					<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
 				<?php endif; ?>
--- themes/madx/page-about.php
+++ themes/madx/page-about.php
@@ -5,7 +5,11 @@ if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 <section class="page-hero" style="background-image: url(<?php the_field('about_hero_background_image'); ?>);">
 
-	<div id="header-grid" class="grid-container">
+	<div class="show-for-small-only">
+		<?php get_template_part('template-parts/menus/about-header-menu'); ?>
+	</div>
+
+	<div id="header-grid" class="grid-container show-for-medium">
 		<div class="grid-x">
 			<div class="small-10 small-offset-1 large-12 large-offset-0">
 				<?php get_template_part('template-parts/menus/about-header-menu'); ?>
