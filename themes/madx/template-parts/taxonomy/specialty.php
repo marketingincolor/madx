273842dc90960e67150642a36a11f02a9286d268
nbeers22 <nbeers22@gmail.com>
2018-11-08 15:32:00 -0600

    Mobile and tablet fixes to home page

--- themes/madx/template-parts/taxonomy/specialty.php
+++ themes/madx/template-parts/taxonomy/specialty.php
@@ -29,9 +29,38 @@ if ($term->slug == 'case-studies' || $term->slug == 'products') {
 			</div>
 		</div>
 		<div class="grid-x">
+			<div class="small-10 small-offset-1 large-12 large-offset-0 cell">
+				<div class="grid-x grid-margin-x grid-margin-y">
 
-			<specialty-industries></specialty-industries>
+				<?php
+					$args = array(
+						'post_type'      => 'specialty',
+						'posts_per_page' => 99,
+						'orderby'        => 'menu_order',
+						'order'          => 'ASC',
+						'tax_query'      => array(
+							array(
+								'taxonomy'   => 'specialty_taxonomies',
+								'field'      => 'slug',
+								'terms'      => 'industries',
+							),
+						),
+					);
+					$query = new WP_Query( $args );
+					while ( $query->have_posts() ) : $query->the_post();
+				?>
 
+					<div class="large-4 medium-6 cell text-center benefit">
+						<?php the_field('specialty_category_icon'); ?>
+						<h5 class="blue"><?php the_title(); ?></h5>
+						<p class="subhead" style="margin-bottom:30px"><?php the_field('specialty_short_description'); ?></p>
+						<p class="text-center"><a href="/specialty-solutions/contact" class="btn-yellow border"><?php _e( 'Start A Project', 'madx' ); ?></a></p>
+					</div>
+
+				<?php endwhile; wp_reset_postdata(); ?>
+
+				</div>
+			</div>
 		</div>
 	</div>
 </section>
