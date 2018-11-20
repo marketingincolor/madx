570810de352015270ef3a8bd658a07df3d0b8048
nbeers22 <nbeers22@gmail.com>
2018-11-20 11:48:49 -0500

    Updated data sheet button on specialty solutions product section

--- themes/madx/template-parts/taxonomy/safety.php
+++ themes/madx/template-parts/taxonomy/safety.php
@@ -37,66 +37,43 @@
 
 <section id="tax-posts" class="taxonomy-products" style="padding-top: 0">
 	<div class="grid-container">
+		<?php if(get_field('products_heading',$term)){ ?>
 		<div class="grid-x">
 			<div class="small-10 large-8 small-offset-1 large-offset-2 text-center">
 				<h2 class="blue"><?php the_field('products_heading',$term); ?></h2>
 				<aside class="yellow-underline center"></aside>
-				<p class="subhead"><?php the_field('products_subhead',$term); ?></p>
+				<?php if(get_field('products_subhead',$term)){ ?><p class="subhead"><?php the_field('products_subhead',$term); ?></p><?php } ?>
 			</div>
 		</div>
+	  <?php } ?>
 
-		<?php if ($term->slug == 'anti-intrusion') { ?>
+		<div class="grid-x">
+			<div class="small-10 small-offset-1 large-12 large-offset-0 cell">
+				<div class="grid-x grid-margin-x grid-margin-y">
 
-			<safety-posts></safety-posts>
+					<?php
+						$args = array(
+							'post_type' => 'safety',
+							'tax_query' => array(
+								array(
+									'taxonomy' => 'safety_taxonomies',
+									'field'    => 'slug',
+									'terms'    => $term->slug
+								),
+							),
+						);
+						$query     = new WP_Query( $args );
+						$num_posts = $query->found_posts;
+						if ($num_posts <= 1) {
+							include(locate_template('/template-parts/taxonomy/safety-security/single-post-layout.php'));
+						}else{
+							include(locate_template('/template-parts/taxonomy/safety-security/multi-post-layout.php'));
+						}
+					?>
 
-		<?php }else{ ?>
-
-		<?php
-			$args = array(
-				'post_type' => 'safety',
-				'tax_query' => array(
-					array(
-						'taxonomy' => 'safety_taxonomies',
-						'field'    => 'slug',
-						'terms'    => $term->slug
-					),
-				),
-			);
-			$query = new WP_Query( $args );
-			while ( $query->have_posts() ) : $query->the_post();
-		?>
-
-			<div class="grid-x">
-				<div id="single-post" class="small-10 small-offset-1 cell module auto-height">
-					<img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
-					<div class="meta">
-						<div class="medium-12 cell">
-							<div class="grid-x grid-margin-x grid-margin-y">
-								<div class="medium-10 medium-offset-1 cell">
-									<h4 class="blue"><?php the_title(); ?></h4>
-									<div class="content"><?php the_content(); ?></div>
-									<?php if (get_field('pdf_link')) { ?>
-										<div class="grid-x grid-margin-y subhead">
-											<div class="medium-2 large-1 cell text-center">
-												<i class="fal fa-file-pdf"></i>
-											</div>
-											<div class="medium-10 cell">
-												<a href="<?php the_field('pdf_link'); ?>" target="_blank">Product Brochure</a>
-												<p>Click to download brochure</p>
-											</div>
-										</div>
-									<?php } ?>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+        </div>
 			</div>
-
-		<?php endwhile; wp_reset_postdata(); ?>
-
-	<?php } ?>
-
+		</div>
 	</div>
 </section>
 
