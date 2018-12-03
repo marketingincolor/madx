66e19fe56ff4b7aa4e2a21c1db1e6218b8e29cae
nbeers22 <nbeers22@gmail.com>
2018-12-03 07:57:55 -0500

    Allowed cross origin requests on site

--- themes/madx/template-parts/menus/residential-header-menu.php
+++ themes/madx/template-parts/menus/residential-header-menu.php
@@ -1,6 +1,6 @@
 <header id="page-header" class="site-header" role="banner">
 	<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle(); ?>>
-		<div class="title-bar-left">
+		<div class="title-bar-left no-print">
 			<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
 			<span class="site-mobile-title title-bar-title">
 				<a href="/residential" rel="home"><?php bloginfo( 'template_directory' ); ?></a>
