c102503e9ca6fb1add148404196b89730cbbbbd2
nbeers22 <nbeers22@gmail.com>
2018-12-03 07:57:55 -0500

    Allowed cross origin requests on site

--- themes/madx/library/rest-api-routes.php
+++ themes/madx/library/rest-api-routes.php
@@ -62,3 +62,20 @@ add_action('rest_api_init', function () {
       'callback' => 'get_current_taxonomy_slug',
     ) );
 });
+
+// Allow cross origin requests during development
+add_action( 'rest_api_init', function() {
+    
+    remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+    add_filter( 'rest_pre_serve_request', function( $value ) {
+        $origin = get_http_origin();
+        if ( $origin ) {
+            header( 'Access-Control-Allow-Origin: ' . esc_url_raw( $origin ) );
+        }
+        header( 'Access-Control-Allow-Origin: ' . esc_url_raw( site_url() ) );
+        header( 'Access-Control-Allow-Methods: GET' );
+
+        return $value;
+        
+    });
+}, 15 );
