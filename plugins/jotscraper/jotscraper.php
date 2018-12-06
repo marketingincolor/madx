14a58051285fc6ed9978ee3f67fb1e357274f25f
nbeers22 <nbeers22@gmail.com>
2018-12-06 12:38:34 -0500

    Completed Sharpspring integration for gated and regular forms

--- plugins/jotscraper/jotscraper.php
+++ plugins/jotscraper/jotscraper.php
@@ -41,26 +41,102 @@ function short_sharp_jot($atts, $content = null){
 
 	$simplehtml = file_get_html("https://form.jotform.com/{$jot_id}",false,null,0);
 	$page_html  = $simplehtml->find('html',0);
-	// $page_head  = $simplehtml->find('head', 0);
-	// $page_form  = $simplehtml->find('form', 0);
+	$page_head  = $simplehtml->find('head', 0);
+	$page_form  = $simplehtml->find('form', 0);
+  $url = $_SERVER['REQUEST_URI'];
+
+  if (strpos($url, 'specialty-solutions/products') !== false) {
+ 
 	$page_ss    = <<<EOT
-	<script>
-		setTimeout(function(){
-			$('#$jot_id').find('button[type=submit]').on('click',function(event){
-				__ss_noform.push(['submit',function(event){window.location = '/success'}, '$ss_id']);
-			});
-		},1000)
-	</script>
 	<script type="text/javascript">
+		var _ss = _ss || [];
     var __ss_noform = __ss_noform || [];
-    __ss_noform.push(['baseURI', 'https://app-3QN6SVTYI8.marketingautomation.services/webforms/receivePostback/MzawMDE1MbQwAgA/']);
-    __ss_noform.push(['form', '$jot_id','$ss_id']);
-    __ss_noform.push(['submitType', 'manual']);
+		var callThisOnReturn = function(resp) {
+			console.log(resp);
+      if(location.href.indexOf('specialty-solutions/products') > -1){
+	      $('.data-sheet').on('click',function(){
+	      	var that = $(this);
+		      if (resp && resp.contact) {
+		      	$('#specialty-pdf-modal').find('iframe').attr('src',$(this).data('pdf'))
+		      	$('#specialty-pdf-modal').foundation('open');
+		      } else{
+						$('#specialty-form-modal').foundation('open');
+						var pdfLink = $(this).data('pdf').split('.com')[1];
+						$('#input_6').val(pdfLink);
+						$('#$jot_id').find('button[type=submit]').on('click',function(event){
+							event.preventDefault();
+							submitForm(pdfLink);
+						});
+		      }
+	      });
+      }
+    };
+    _ss.push(['_setResponseCallback', callThisOnReturn]);
+    _ss.push(['_setDomain', 'https://koi-3QN6SVTYI8.marketingautomation.services/net']);
+    _ss.push(['_setAccount', 'KOI-42GDPGWMYW']);
+		_ss.push(['_trackPageView']);
+    (function() {
+        var ss = document.createElement('script');
+        ss.type = 'text/javascript'; ss.async = true;
+        ss.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'koi-3QN6SVTYI8.marketingautomation.services/client/ss.js?ver=1.1.1';
+        var scr = document.getElementsByTagName('script')[0];
+        scr.parentNode.insertBefore(ss, scr);
+    })();
+    window.onload = function(){
+	    __ss_noform.push(['baseURI', 'https://app-3QN6SVTYI8.marketingautomation.services/webforms/receivePostback/MzawMDE1MbQwAgA/']);
+	    __ss_noform.push(['form', '$jot_id','$ss_id']);
+	    __ss_noform.push(['submitType', 'manual']);
+    }; 
+    
 	</script>
 	<script type="text/javascript" src="https://koi-3QN6SVTYI8.marketingautomation.services/client/noform.js?ver=1.24"></script>
+	<script>
+
+		function submitForm(pdfLink){
+			$.ajax({
+				type: 'POST',
+				success: function(data){
+				  if(location.href.indexOf('specialty-solutions/products') > -1){
+				  	function goToPdf(){
+				  		$('#specialty-form-modal').foundation('close');
+							$('#specialty-pdf-modal').find('iframe').attr('src',pdfLink)
+							$('#specialty-pdf-modal').foundation('open');
+				  	}
+						__ss_noform.push(['submit',null, '$ss_id']);
+						goToPdf();
+				  }else{
+				  	__ss_noform.push(['submit',function(event){window.location = '/success'}, '$ss_id']);
+				  }
+				}
+			});
+		}
+	</script>
+EOT;
+
+  $form_output = $page_ss . $page_html;
+
+}else{
+
+	$page_ss = <<<EOT
+		<script>
+			setTimeout(function(){
+				$('#$jot_id').find('button[type=submit]').on('click',function(event){
+					__ss_noform.push(['submit',function(event){window.location = '/success'}, '$ss_id']);
+				});
+			},1000)
+		</script>
+		<script type="text/javascript">
+	    var __ss_noform = __ss_noform || [];
+	    __ss_noform.push(['baseURI', 'https://app-3QN6SVTYI8.marketingautomation.services/webforms/receivePostback/MzawMDE1MbQwAgA/']);
+	    __ss_noform.push(['form', '$jot_id','$ss_id']);
+	    __ss_noform.push(['submitType', 'manual']);
+		</script>
+		<script type="text/javascript" src="https://koi-3QN6SVTYI8.marketingautomation.services/client/noform.js?ver=1.24"></script>
 EOT;
 
 	$form_output = $page_html . $page_ss;
+}
+
 	return $form_output;
 
 }
--- themes/madx/header.php
+++ themes/madx/header.php
@@ -9,22 +9,24 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-		<!-- SS -->
-		<script type="text/javascript">
-		    var _ss = _ss || [];
-		    _ss.push(['_setDomain', 'https://koi-3QN6SVTYI8.marketingautomation.services/net']);
-		    _ss.push(['_setAccount', 'KOI-42GDPGWMYW']);
-		    _ss.push(['_trackPageView']);
-		(function() {
-		    var ss = document.createElement('script');
-		    ss.type = 'text/javascript'; ss.async = true;
-		    ss.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'koi-3QN6SVTYI8.marketingautomation.services/client/ss.js?ver=1.1.1';
-		    var scr = document.getElementsByTagName('script')[0];
-		    scr.parentNode.insertBefore(ss, scr);
-		})();
-		</script>
-		<!-- SS -->
+		<?php $url = $_SERVER['REQUEST_URI']; ?>
+		<?php if (strpos($url, 'specialty-solutions/products') === false) { ?>
+			<!-- SS -->
+			<script type="text/javascript">
+			    var _ss = _ss || [];
+			    _ss.push(['_setDomain', 'https://koi-3QN6SVTYI8.marketingautomation.services/net']);
+			    _ss.push(['_setAccount', 'KOI-42GDPGWMYW']);
+			    _ss.push(['_trackPageView']);
+			(function() {
+			    var ss = document.createElement('script');
+			    ss.type = 'text/javascript'; ss.async = true;
+			    ss.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'koi-3QN6SVTYI8.marketingautomation.services/client/ss.js?ver=1.1.1';
+			    var scr = document.getElementsByTagName('script')[0];
+			    scr.parentNode.insertBefore(ss, scr);
+			})();
+			</script>
+			<!-- SS -->
+	  <?php } ?>
 		
 		<?php wp_head(); ?>
 	</head>
