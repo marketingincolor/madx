bb7d2bd43abc84ab29a72e5a801f762b4a4d1590
nbeers22 <nbeers22@gmail.com>
2018-12-18 16:40:31 -0500

    Added classes to home modules and menu images for analytics

--- themes/madx/header-madicou.php
+++ themes/madx/header-madicou.php
@@ -71,10 +71,10 @@
 			</div>
 			<div class="top-bar-right">
 				<ul class="menu">
-					<li><a href="/madicou"><i class="icon-madico-u-icon"></i>&nbsp; <?php _e( 'Madico U', 'madx' ); ?></a></li>
-					<li><a href="/dealer-portal"><i class="fas fa-id-badge"></i>&nbsp; <?php _e( 'Dealer Portal', 'madx' ); ?></a></li>
+					<li><a href="/madicou" class="header-menu-madicou"><i class="icon-madico-u-icon"></i>&nbsp; <?php _e( 'Madico U', 'madx' ); ?></a></li>
+					<li><a href="/dealer-portal" class="header-menu-dealer-portal"><i class="fas fa-id-badge"></i>&nbsp; <?php _e( 'Dealer Portal', 'madx' ); ?></a></li>
 		      <li>
-		        <a data-toggle="search-dropdown"><i class="fas fa-search"></i>&nbsp; <?php _e( 'Search', 'madx' ); ?></a>
+		        <a data-toggle="search-dropdown" class="header-menu-search"><i class="fas fa-search"></i>&nbsp; <?php _e( 'Search', 'madx' ); ?></a>
 		        <ul class="menu vertical">
 		          <li class="dropdown-pane right" id="search-dropdown" data-position="bottom" v-drop-click>
 								<?php get_template_part('template-parts/search/page-searchform'); ?>
--- themes/madx/template-parts/menus/protectionpro-header-menu.php
+++ themes/madx/template-parts/menus/protectionpro-header-menu.php
@@ -3,7 +3,7 @@
 		<div class="title-bar-left">
 			<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
 			<span class="site-mobile-title title-bar-title">
-				<a href="/protectionpro" rel="top"><?php bloginfo( 'template_directory' ); ?></a>
+				<a href="/protectionpro" rel="top" class="menu-section-home-image"><?php bloginfo( 'template_directory' ); ?></a>
 			</span>
 		</div>
 	</div>
@@ -11,7 +11,7 @@
 	<nav class="site-navigation top-bar" role="navigation">
 		<div class="top-bar-left">
 			<div class="site-desktop-title top-bar-title">
-				<a href="/protectionpro" rel="top"><img src="<?php bloginfo( 'template_directory' ); ?>/dist/assets/images/protectionpro-logo.png" alt="ProtectionPro by Madico"></a>
+				<a href="/protectionpro" rel="top"><img src="<?php bloginfo( 'template_directory' ); ?>/dist/assets/images/protectionpro-logo.png" alt="ProtectionPro by Madico" class="menu-section-home-image"></a>
 			</div>
 		</div>
 		<div class="top-bar-right">
